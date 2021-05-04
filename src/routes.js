d9b267f9bd15cbb2ba835ff65d28d47a03fa0c14
Paulo Pimentel <paulo.h.g.pimentel@gmail.com>
2021-05-03 22:57:02 -0300

    :tada: Atualizando arquivos de rotas e html para adicionar novos sons

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const routes = express.Router()
 
-routes.get('/sounds/background-music', (req, resp) => {
-    return resp.sendFile(`${__dirname}/views/sound/background-music.html`);
+routes.get('/sounds/title', (req, resp) => {
+    return resp.sendFile(`${__dirname}/views/sound/title.html`);
+});
+
+routes.get('/sounds/item-coletado', (req, resp) => {
+    return resp.sendFile(`${__dirname}/views/sound/item-coletado.html`);
+});
+
+routes.get('/sounds/decifrar-codigo', (req, resp) => {
+    return resp.sendFile(`${__dirname}/views/sound/decifrar-codigo.html`);
+});
+
+routes.get('/sounds/porta-entrar', (req, resp) => {
+    return resp.sendFile(`${__dirname}/views/sound/porta-entrar.html`);
+});
+
+routes.get('/sounds/porta-sair', (req, resp) => {
+    return resp.sendFile(`${__dirname}/views/sound/porta-sair.html`);
 });
 
 module.exports = routes
--- src/server.js
+++ src/server.js
@@ -2,9 +2,13 @@ const express = require("express")
 const server = express()
 const routes = require("./routes")
 const port = process.env.PORT||3000
+const cors = require("cors")
 
 // habilitar arquivos statics
 server.use(express.static("public"))
+
+//cors
+server.use(cors())
 
 //routes
 server.use(routes)
